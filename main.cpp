fa3dcc01de707c4b081173b662e7629198e1f879
shayne2000 <126381922+Shayne2000@users.noreply.github.com>
2026-04-20 09:35:19 +0000

    puch 1     จารย์ไม่อ่าน chat

--- main.cpp
+++ main.cpp
@@ -1,33 +1,50 @@
 #include <iostream>
 
+class myf1 : public std::exception {
+	public :
+		virtual const char* what() const throw(){
+			return "dev by 0" ;
+		}
+};
+
+class myf2 : public std::exception {
+	public :
+		virtual const char* what() const throw(){
+			std::cin.clear();
+            std::cin.ignore(50,'\n');
+			return "not number" ;
+		}
+};
+
 int main(){
 	int x,y;
 	double d;
+	int pass = 0;
+	do{
+		pass = 1 ;
+		try {
+			std::cin>>x>>y;
+		
+			if(std::cin.fail()){
+				myf2 f2 ;
+				throw f2 ;
+			}
+			if(abs(x)>1000 ||abs(y)>1000){
+				throw("alue out of range");
+			}
+			if (y==0) {
+				myf1 f1;
+				throw f1;
+			}
+			d=(double) x/y;
+			std::cout<< "The result is " <<d<<std::endl;	
+		}
 
-	try {
-		for (int i = 0 ; i < 100 ; i ++) {
-			new int[1000000000000000];
-			std::cout<<"test"<<i<<std::endl;
+		catch(std::exception &e){
+			std::cout<<"std error : "<<e.what()<<std::endl;
+			pass = 0 ;
 		}
-		std::cin>>x>>y;
-	
-		if(std::cin.fail()){
-			throw("Incorrect type entered");
-		}
-		if(abs(x)>1000 ||abs(y)>1000){
-			throw("alue out of range");
-		}
-		if (y==0) {
-			throw("Error divide by zero");
-		}
-		d=(double) x/y;
-		std::cout<< "The result is " <<d<<std::endl;	
-	} catch(const char* c) {
-		std::cerr << c << std:: endl;
-	}
-	catch(...){
-		std::cout<<"mem??"<<std::endl;
-	}
+	}while (pass == 0);
 
 	
 	return 0;
