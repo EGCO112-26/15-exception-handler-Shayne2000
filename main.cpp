419d5fd148fba178856b0616109af8c4685ace50
shayne2000 <126381922+Shayne2000@users.noreply.github.com>
2026-04-20 08:23:37 +0000

    random sht goooo brrrr

--- main.cpp
+++ main.cpp
@@ -1,18 +1,30 @@
+#include <iostream>
+
 int main(){
 	int x,y;
 	double d;
-	cin>>x>>y;
-	if(cin.fail()){
-		cerr<<"Incorrect type entered"; return 1;
-	}
-	if(abs(x)>1000 ||abs(y)>1000){
-		cerr<<"alue out of range"; return 1;
-	}
-	if (y==0) {
-		cerr<< "Error divide by zero"; return 1;
+
+	try {
+		for (int i = 0 ; i < 100000000 ; i ++) {
+			new int;
+		}
+		std::cin>>x>>y;
+	
+		if(std::cin.fail()){
+			throw("Incorrect type entered");
+		}
+		if(abs(x)>1000 ||abs(y)>1000){
+			throw("alue out of range");
+		}
+		if (y==0) {
+			throw("Error divide by zero");
+		}
+		d=(double) x/y;
+		std::cout<< "The result is " <<d<<std::endl;	
+	} catch(const char* c) {
+		std::cerr << c << std:: endl;
 	}
 
-	d=(double) x/y;
-	cout<< "The result is" <<d;
+	
 	return 0;
 }
